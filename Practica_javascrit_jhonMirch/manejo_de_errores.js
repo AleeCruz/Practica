566a5431b4cd37dbaf2461699b3d5961fbf69c18
Alexander <acruz@fi.uba.ar>
2026-02-11 17:44:09 -0300

    Agregado: Se agrego el ejercicio de errores

--- Practica_javascrit_jhonMirch/manejo_de_errores.js
+++ Practica_javascrit_jhonMirch/manejo_de_errores.js
@@ -1,33 +1,58 @@
-console.log("\n\nVeremos los manejos de erores en javascript desde cero----------\n\n")
+console.log("\n\n----------Vamos a ver el amnejo de errores con la sentenicias try-catch----------\n\n")
 
 
 
+/*
+try {
+    console.log("Aca va a ir el codigo a evaluar dentro del bloque try se ejecura")
+    console.log("\n\nSoy el segundo mensaje del trycatch")
+    console.log("\n\n")
 
-//Manejos de errores 
+    //Vamos a ejecutar un error de manera voluntaria 
+    //veremos como sera el comprtamiento del try-catch
+   // Errorquenoesixte;
+
+
+    console.log("Soy el ultimo mensaje del bloque try")
+} catch (error) {
+
+    //Esta seccion se ejecutara si se encuantra algun errro en el bloque try
+    console.log("\n\n")
+    console.log("Esta seccion de codigo se va a ejecutar si ocurrio algun error")
+
+    console.log("EL tipo de error es: \n")
+    console.log(error)
+
+
+    console.log("\n\n")
+
+}finally{
+    console.log("\n\nEl bloque finally siempre se ejecutara al final de un bloque try-catch")
+}
+
+*/
+
+
+
+//Vamos a analizar otro bllque del tipor try-catch
 
 
 try {
-    //Aca va a ir el codigo necesario para manejar exepciones
 
-    //En caso de que exista un error el codigo trycatch lanzara una excepcions 
+    let numero = "ale";
 
+    if (isNaN(numero)){
+        throw new Error("El valor ingresado no es un numero")
+    }
+
+    console.log("Esta linea de codigo se ejecutar porque es un numero: "+numero*numero)
     
-    //Este es un error de javascript desde cero 
-
-
-    console.log("Estamos dentro del try ")
-
-    WDWAFWEEW
-    WFWEFW
-
-    
-    console.log("Estamos viendo la parte del try todavia")
-
-
 } catch (error) {
-    console.log("Acaba de aparecer un error del tipo: "+error)
-}finally{
-
-    //Este bloque de codigo se ejecutara siempre 
-    console.log("\n\n-Esta seccion del codigo se ejecutara siempre porque no importa si existe o no un error-\n\n")
+    console.log("Se produjo la siguiente excepcion: "+error)   
 }
+
+
+
+
+//Logramos analizar el manejo de excepsiones en javascript ahora podremos mandar mensajes
+//De error perzonalizados 
