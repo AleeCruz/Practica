b7823a2ef1a3ae3187d69058b755847d00ce4ada
Alexander <acruz@fi.uba.ar>
2026-02-11 14:29:55 -0300

    Agregado: Estamos viendo los errores en javascript

---
+++ Practica_javascrit_jhonMirch/manejo_de_errores.js
@@ -0,0 +1,33 @@
+console.log("\n\nVeremos los manejos de erores en javascript desde cero----------\n\n")
+
+
+
+
+//Manejos de errores 
+
+
+try {
+    //Aca va a ir el codigo necesario para manejar exepciones
+
+    //En caso de que exista un error el codigo trycatch lanzara una excepcions 
+
+    
+    //Este es un error de javascript desde cero 
+
+
+    console.log("Estamos dentro del try ")
+
+    WDWAFWEEW
+    WFWEFW
+
+    
+    console.log("Estamos viendo la parte del try todavia")
+
+
+} catch (error) {
+    console.log("Acaba de aparecer un error del tipo: "+error)
+}finally{
+
+    //Este bloque de codigo se ejecutara siempre 
+    console.log("\n\n-Esta seccion del codigo se ejecutara siempre porque no importa si existe o no un error-\n\n")
+}
