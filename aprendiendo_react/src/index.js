80121004998861e5f09342a01be66cbf38f63b97
Alexander <acruz@fi.uba.ar>
2026-02-12 16:31:03 -0300

    Agregado: Se agregaron los archivos correspondientes a los ejercicios 16 y 17

--- aprendiendo_react/src/index.js
+++ aprendiendo_react/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Importas desde /client
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Hola Mundo</h1>
-  </React.StrictMode>
+
+//Para trabajar con JSX debes de colocar todo dentro de una etiqueta el JSX cambia 
+
+//Veremos un par de cosas de codigo jsx
+const nombre = "Alexander"
+const JSX = (
+  //Podriamos poner clases a ciertas etiquetas
+    <>
+      <h1 className=''>Hola Mundo te saluda {nombre}</h1>
+      <p>Esto es un parrafo para el uso de react</p>
+    </>
+    /**Esto es totalmente funcional
+    <div>
+      <h1>Hola Mundo te saluda {nombre}</h1>
+      <p>Esto es un parrafo para el uso de react</p>
+    </div>
+     */
 );
 
+// 1. Creas la raíz apuntando al elemento 'root'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 2. Renderizas el JSX usando el método render de esa raíz
+root.render(JSX);
