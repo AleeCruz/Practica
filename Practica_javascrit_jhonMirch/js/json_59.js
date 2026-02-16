341e90188c6d1425b0d03cca7a38451fa6cbbedd
Alexander <acruz@fi.uba.ar>
2026-02-16 03:06:25 -0300

    Agregado: Se incorporo Firebase + cloudinary

---
+++ Practica_javascrit_jhonMirch/js/json_59.js
@@ -0,0 +1,37 @@
+/**
+ * Hablaremos sobre JSON que trata sobre javascript Object Notations
+ * 
+ * QUe es exactamente ?
+ * 
+ * Se trata de un formato ligero de intercambio de datos  es decir : 
+ */
+
+
+
+//Vamos a ver un objeto en javascript y luego vamos a cer el formato JSON
+
+
+const json ={
+
+    cadena: "jon",
+    numero:35,
+    boolean: true,
+    arreglo: ["correr","programar","cocinar"],
+    objeto:{
+        twiter: "@jhonmircha",
+        email:"usuarionuevo@gmail.com"
+    },
+    nulo:null
+}
+
+
+//Vamos a mostrarlo  de la siguiente manera
+
+console.log(json)
+
+
+
+//vamos a ver JSON y 2 metodos importantes que una persona debe de conocerlo
+
+
+console.log(JSON)
